26d19cfc97f37428b57da89e7e84785234b780b3
sawabe tomohiro <sawabe.tomohiro.jri@outlook.jp>
2020-06-02 21:34:26 +0900

    learn JSX Syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  // const greeting = "Hi, Tom!"
+  // const dom = <h1 className="foo">{greeting}</h1>;
+  const dom = (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("Test!")}}></input>
+    </React.Fragment>
+  )
+  return dom;
 }
 
 export default App;
