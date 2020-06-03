1df50d60f443a1b82889ee5db0ac1ddae4b7ec5a
sawabe tomohiro <sawabe.tomohiro.jri@outlook.jp>
2020-06-03 15:50:50 +0900

    learn Function / Class Components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 function App() {
   // const greeting = "Hi, Tom!"
   // const dom = <h1 className="foo">{greeting}</h1>;
   const dom = (
     <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("Test!")}}></input>
+      {/* <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("Function Component")}}></input> */}
+      <Cat />
+      <Cat />
     </React.Fragment>
   )
   return dom;
+}
+
+
+function Cat() {
+  return <div>Meow!</div>
+}
+
+class App2 extends Component {
+  render() {
+    return (
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("Class Component")}}></input>
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
